e93c7bbfae0c51afa11c93155b775480ea820085
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-03-14 02:04:36 -0400

    Update __init__.py

--- AutoEmbed/__init__.py
+++ AutoEmbed/__init__.py
@@ -5,5 +5,5 @@ from .autoembed import AutoEmbed
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot: commands.Bot):
-    n = AutoEmbed(bot)
-    await bot.add_cog(n)
+    cog = AutoEmbed(bot)
+    await bot.add_cog(cog)
