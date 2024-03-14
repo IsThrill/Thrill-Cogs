9f5c635c8cc4afcfcc2f9f6033fe8d98077f46e2
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-03-14 01:55:06 -0400

    Update __init__ - More hotfixes

--- AutoEmbed/__init__.py
+++ AutoEmbed/__init__.py
@@ -1,9 +1,9 @@
 from redbot.core import commands
 
-from .autoembeder import AutoEmbeder
+from .autoembed import AutoEmbed
 
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot: commands.Bot):
-    n = AutoEmbeder(bot)
+    n = AutoEmbed(bot)
     await bot.add_cog(n)
