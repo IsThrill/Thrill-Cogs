a861babc30cd82e4a8c5c3a377f6d4f4d67eed27
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-12-08 22:44:14 -0500

    Delete AutoEmbed directory

--- AutoEmbed/__init__.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-from .autoembed import AutoEmbed
-
-__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
-
-async def setup(bot: commands.Bot):
-    cog = AutoEmbed(bot)
-    await bot.add_cog(cog)
