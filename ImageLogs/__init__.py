4f01fbe461e533056f3dc4b7164d7e1946f12224
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-12-08 23:22:33 -0500

    Update __init__.py

--- ImageLogs/__init__.py
+++ ImageLogs/__init__.py
@@ -3,8 +3,6 @@ from redbot.core import commands
 from .imagedeletelogger import ImageDeleteLogger
 
 async def setup(bot: commands.Bot):
-
+    """Setup function for the cog, adding it to the bot."""
     image_logger = ImageDeleteLogger(bot)
-    
-    # Add the cog to the bot
     await bot.add_cog(image_logger)
