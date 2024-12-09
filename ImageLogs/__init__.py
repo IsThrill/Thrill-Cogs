c4d42583eb665f49cb729541e0d6d5a446d78b7a
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-12-09 18:15:37 -0500

    Update __init__.py

--- ImageLogs/__init__.py
+++ ImageLogs/__init__.py
@@ -6,6 +6,6 @@ from .imagelogs import ImageLogs
 
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
-async def setup(bot: Red):
+async def setup(bot):
     cog = ImageLogs(bot)
     await bot.add_cog(cog)
