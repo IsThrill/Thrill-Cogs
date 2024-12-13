b11a7516bef5cc1ca58c797e06d72a0331974105
IsThrill <45497437+IsThrill@users.noreply.github.com>
2024-12-12 23:25:11 -0500

    Update smelly.py

--- SmellyDetector/smelly.py
+++ SmellyDetector/smelly.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 from redbot.core import commands
 import random
 
@@ -9,18 +8,13 @@ class Smelly(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"Cog {self.__class__.__name__} is ready.")
-
-    @commands.slash_command(name="smelly", description="Find out how smelly you are!")
-    async def smelly(self, ctx: discord.ApplicationContext):
-        """Slash command to determine your smelliness."""
+    @commands.command(name="smelly")
+    async def smelly(self, ctx: commands.Context):
+        """Find out how smelly you are."""
         smelliness = random.randint(0, 100)
         embed = discord.Embed(
             title="Smelliness Detector",
-            description=f"{ctx.author.mention}, your smelliness level is **{smelliness}%**! 👃",
+            description=f"{ctx.author.mention}, your smelliness level is **{smelliness}%**! \ud83d\udc43",
             color=discord.Color.random()
         )
-        await ctx.respond(embed=embed)
-
+        await ctx.send(embed=embed)
