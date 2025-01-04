db20697155ad0c4b9c12ddafaefc5f540176ef47
IsThrill <45497437+IsThrill@users.noreply.github.com>
2025-01-04 13:08:14 -0500

    Delete Suspicious System directory

--- Suspicious System/__init__.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-from redbot.core.bot import Red
-from .suspicioususermonitor import SuspiciousUserMonitor
-
-__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
-
-async def setup(bot: Red):
-    cog = SuspiciousUserMonitor(bot)
-    await bot.add_cog(cog)
