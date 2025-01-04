55bf3cc0701925310220fd9e875493da1eb6d3ea
IsThrill <45497437+IsThrill@users.noreply.github.com>
2025-01-04 13:03:04 -0500

    Update __init__.py

--- Suspicious System/__init__.py
+++ Suspicious System/__init__.py
@@ -1,4 +1,9 @@
-from .suspicioususermonitor import SuspiciousUserMonitor
+from redbot.core import commands
+from redbot.core.bot import Red
+from .suspicious_user_monitor import SuspiciousUserMonitor
 
-async def setup(bot):
-    await bot.add_cog(SuspiciousUserMonitor(bot))
+__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
+
+async def setup(bot: Red):
+    cog = SuspiciousUserMonitor(bot)
+    await bot.add_cog(cog)
