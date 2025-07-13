0bfcdcca5bb30c33d8746539d2b9bfa7a7272fdc
IsThrill <45497437+IsThrill@users.noreply.github.com>
2025-07-13 15:10:40 -0400

    Update voice.py

--- ThrillsRobustLogging/listeners/voice.py
+++ ThrillsRobustLogging/listeners/voice.py
@@ -25,26 +25,41 @@ class VoiceListeners(commands.Cog):
         guild = member.guild
         embed = None
 
+        # Member joins a voice channel
         if not before.channel and after.channel:
             embed = await logembeds.voice_joined(member, after.channel)
             await self.cog._send_log(guild, embed, "voice", "join")
 
+        # Member leaves a voice channel
         elif before.channel and not after.channel:
             embed = await logembeds.voice_left(member, before.channel)
             await self.cog._send_log(guild, embed, "voice", "leave")
 
+        # Member moves between voice channels
         elif before.channel and after.channel and before.channel != after.channel:
             embed = await logembeds.voice_moved(member, before.channel, after.channel)
             await self.cog._send_log(guild, embed, "voice", "move")
 
+        # Member is server-muted or unmuted
         elif before.mute != after.mute:
             channel = after.channel or before.channel
             if channel:
-                embed = await logembeds.voice_server_muted(member, channel, after.mute)
+                entry = await self.cog._get_audit_log_entry(guild, member, discord.AuditLogAction.member_update)
+                moderator = "Unknown"
+                if entry and entry.before.mute != entry.after.mute:
+                    moderator = entry.user
+
+                embed = await logembeds.voice_server_muted(member, channel, moderator, after.mute)
                 await self.cog._send_log(guild, embed, "voice", "mute")
             
+        # Member is server-deafened or undeafened
         elif before.deaf != after.deaf:
             channel = after.channel or before.channel
             if channel:
-                embed = await logembeds.voice_server_deafened(member, channel, after.deaf)
+                entry = await self.cog._get_audit_log_entry(guild, member, discord.AuditLogAction.member_update)
+                moderator = "Unknown"
+                if entry and entry.before.deaf != entry.after.deaf:
+                    moderator = entry.user
+                
+                embed = await logembeds.voice_server_deafened(member, channel, moderator, after.deaf)
                 await self.cog._send_log(guild, embed, "voice", "deafen")
